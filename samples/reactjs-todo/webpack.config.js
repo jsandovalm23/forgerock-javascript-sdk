64ee2d07fbd5249e90f35b9e1fd588c48a38b0f3
Ryan Bas <ryan.basmajian@forgerock.com>
2022-05-18 12:34:53 -0600

    ci: preview-environments
    Adds the okteto preview environment ci files to the pipeline

--- samples/reactjs-todo/webpack.config.js
+++ samples/reactjs-todo/webpack.config.js
@@ -2,19 +2,25 @@ const webpack = require('webpack');
 
 module.exports = (config) => {
   const AM_URL = process.env.AM_URL;
-  const APP_URL = process.env.APP_URL;
   const API_URL = process.env.API_URL;
   const DEBUGGER_OFF = process.env.DEBUGGER_OFF;
   const JOURNEY_LOGIN = process.env.JOURNEY_LOGIN;
   const JOURNEY_REGISTER = process.env.JOURNEY_REGISTER;
   const WEB_OAUTH_CLIENT = process.env.WEB_OAUTH_CLIENT;
   const REALM_PATH = process.env.REALM_PATH;
-
+  console.log({
+    AM_URL,
+    API_URL,
+    DEBUGGER_OFF,
+    JOURNEY_LOGIN,
+    JOURNEY_REGISTER,
+    WEB_OAUTH_CLIENT,
+    REALM_PATH,
+  });
   config.plugins.push(
     new webpack.DefinePlugin({
       // Inject all the environment variable into the Webpack build
       'process.env.AM_URL': JSON.stringify(AM_URL),
-      'process.env.APP_URL': JSON.stringify(APP_URL),
       'process.env.API_URL': JSON.stringify(API_URL),
       'process.env.DEBUGGER_OFF': JSON.stringify(DEBUGGER_OFF),
       'process.env.JOURNEY_LOGIN': JSON.stringify(JOURNEY_LOGIN),
